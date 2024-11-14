0fb4703e43e91ca5ae5dc2fda9177e842c4bbee8
domNetco <dominic.beardmore1@nhs.net>
2024-11-14 15:47:23 +0000

    Tab navigator and some styling

--- apps/app/src/app/(app)/(tabs)/_layout.tsx
+++ apps/app/src/app/(app)/(tabs)/_layout.tsx
@@ -1,14 +1,23 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { View } from 'react-native';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'white',
+        tabBarActiveBackgroundColor: '#015EB8',
+        tabBarInactiveBackgroundColor: '#015EB8',
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
+          headerShown: false,
           tabBarIcon: ({ color }) => (
             <FontAwesome size={28} name="home" color={color} />
           ),
@@ -18,9 +27,42 @@ export default function TabLayout() {
         name="credentials"
         options={{
           title: 'Credentials',
+          headerShown: false,
           tabBarIcon: ({ color }) => (
             <FontAwesome size={28} name="cog" color={color} />
           ),
+        }}
+      />
+      <Tabs.Screen
+        name="connections"
+        options={{
+          title: 'Connections',
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="handshake-o" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="messages"
+        options={{
+          title: 'Messages',
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="envelope-open" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="share/index"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="issue/index"
+        options={{
+          href: null,
         }}
       />
     </Tabs>
