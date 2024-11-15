63b03254571a2e2951068f337ea4b4b8f5f6705b
domNetco <dominic.beardmore1@nhs.net>
2024-11-15 15:42:00 +0000

    Added the NHS logo

--- apps/app/src/app/landing.tsx
+++ apps/app/src/app/landing.tsx
@@ -1,6 +1,7 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { Button } from '@digital-staff-passport/ui';
 import { useAuth } from '@digital-staff-passport/features';
+import NHS_LOGO from '../../assets/NHS_Logo.svg';
 
 export default function Landing() {
   const { toggleLogin } = useAuth();
@@ -8,9 +9,13 @@ export default function Landing() {
     <View
       style={{ flex: 1, backgroundColor: '#015EB8', alignContent: 'center' }}
     >
-      <View style={{ alignItems: 'center' }}>
+      <View style={{ flex: 6, alignItems: 'center', justifyContent: 'center' }}>
+        <NHS_LOGO width={500} height={50} />
+      </View>
+      <View style={{ flex: 3 }}>
         <Text
           style={{
+            paddingBottom: 10,
             color: 'white',
             fontSize: '30',
             fontWeight: 'bold',
@@ -19,20 +24,19 @@ export default function Landing() {
         >
           Access your NHS Staff Passport
         </Text>
-      </View>
-
-      <Button title="Continue" onPress={toggleLogin} action="primary" />
-      <View style={{ alignItems: 'center' }}>
-        <Text
-          style={{
-            color: 'white',
-            fontSize: '20',
-            fontWeight: 'bold',
-            textAlign: 'center',
-          }}
-        >
-          version 1.0
-        </Text>
+        <Button title="Continue" onPress={toggleLogin} action="primary" />
+        <View style={{ alignItems: 'center', paddingTop: 50 }}>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: '20',
+              fontWeight: 'bold',
+              textAlign: 'center',
+            }}
+          >
+            version 1.0
+          </Text>
+        </View>
       </View>
     </View>
   );
